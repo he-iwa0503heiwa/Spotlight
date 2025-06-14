49d1731d42379310979193987ac8483d176bc912
he-iwa0503heiwa <heiwa.2012.nin@gmail.com>
2025-06-14 22:47:54 +0900

    modi EventShareAppApplicationTests.java

--- app/src/test/java/com/eventshare/app/EventShareAppApplicationTests.java
+++ app/src/test/java/com/eventshare/app/EventShareAppApplicationTests.java
@@ -1,19 +1,16 @@
 package com.eventshare.app;
 
-import com.eventshare.app.config.TestSecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@Import(TestSecurityConfig.class)
 @ActiveProfiles("test")
 class EventShareAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-		//これから設定
-	}
+    @Test
+    void contextLoads() {
+        //これから設定
+    }
 
 }
