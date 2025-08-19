6f9817ec1039dce44eb7582da6ffcc9e6ba77098
he-iwa0503heiwa <heiwa.2012.nin@gmail.com>
2025-08-19 23:10:24 +0900

    testGetAllEvents

--- app/src/test/java/com/eventshare/app/controller/EventServiceImplTest.java
+++ app/src/test/java/com/eventshare/app/controller/EventServiceImplTest.java
@@ -6,12 +6,18 @@ import com.eventshare.app.entity.User;
 import com.eventshare.app.repository.EventRepository;
 import com.eventshare.app.service.impl.EventServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 /*
 EventServiceImplのテストクラス
@@ -50,5 +56,20 @@ public class EventServiceImplTest {
         testEvent.setCapacity(50);
         testEvent.setCategory(testCategory);
         testEvent.setCreator(testUser);
+    }
+
+    @Test
+    void testGetAllEvents() {
+        //モックの設定
+        List<Event> events = Arrays.asList(testEvent);
+        when(eventRepository.findAll()).thenReturn(events);
+
+        //テスト実行
+        List<Event> result = eventService.getAllEvents();
+
+        //検証
+        assertEquals(1, result.size());//サイズが１か
+        assertEquals("", result.get(0).getTitle());//タイトル正しいか
+        verify(eventRepository, times(1)).findAll();//findAll()が呼ばれたのは1回か
     }
 }
